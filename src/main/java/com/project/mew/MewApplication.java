ad1d696fb270ebf5d7127833007f4a07a9222088
Hyewon <hwwwon1027@gmail.com>
2022-12-16 20:00:11 +0900

    Initial commit

---
+++ src/main/java/com/project/mew/MewApplication.java
@@ -0,0 +1,13 @@
+package com.project.mew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mew/MewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
