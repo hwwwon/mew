bf61f86af234fa41256a0aa8cc3e378d945e4931
Hyewon <hwwwon1027@gmail.com>
2023-01-16 22:59:06 +0900

    "movieSearch 기록 남기기 디테일 기능(누구랑, 장소, 앨범선택, 평가) jsp, js, css"

--- src/main/resources/static/js/movieSearch.js
+++ src/main/resources/static/js/movieSearch.js
@@ -18,6 +18,8 @@ topBtn.addEventListener('click', (e) => {
 const pickBtn = document.querySelector('.pick-btn');
 const starImg = document.querySelector('.pick-btn img');
 const recordBtn = document.querySelector('.record-btn');
+const closeBtn = document.querySelector('.record-btn-cancel');
+const inputReset = document.querySelectorAll('.watch-info input');
 pickBtn.addEventListener('click', () => {
     starImg.classList.toggle('yellow-star');
     if(starImg.classList.contains('yellow-star')) alert("나중에 볼 영화에 저장되었습니다. 마이페이지에서 확인해 보세요.");
@@ -30,10 +32,14 @@ recordBtn.addEventListener('click', () => {
     recordArea.classList.add('record-toggle');
 });
 // 영화 기록 팝업창 취소(닫기)
-document.querySelector('.record-btn-cancel').addEventListener('click', () => {
-    if(confirm('저장하지 않고 취소 하시겠습니까?')){
-        recordArea.classList.remove('record-toggle');
-    }else return false;
+closeBtn.addEventListener('click', () => {
+    if(recordBtn.textContent === "기록 남기기"){
+        if(confirm('저장하지 않고 취소하시겠습니까?')){
+            recordArea.classList.remove('record-toggle');
+            inputReset.forEach(item => item.value="");
+        }else return false;
+    }
+    recordArea.classList.remove('record-toggle');
 });
 // 영화 기록 팝업창 저장
 document.querySelector('.record-btn-submit').addEventListener('click', () => {
@@ -41,9 +47,10 @@ document.querySelector('.record-btn-submit').addEventListener('click', () => {
     alert(`기본 앨범에 저장되었습니다. 마이페이지에서 확인해 보세요.`);
     recordBtn.innerText = "기록 완료";
     recordBtn.style.color = "#7fe5ff";
+    closeBtn.innerText = "닫기";
 });
 
-// bootstrap datetimepicker jquery
+// 영화 시청 날짜 및 시간 선택 bootstrap datetimepicker jquery
 $(document).ready(function()
 {
     $('#movieWatchDate').bootstrapMaterialDatePicker({
@@ -54,4 +61,182 @@ $(document).ready(function()
     });
 
 });
+
+// 관람장소, 누구랑, 평점, 앨범선택 팝업창
+const watchDetail = document.querySelectorAll('.watch-detail');
+const detailArea = document.querySelectorAll('.record-detail-area');
+let siblings = t => [...t.parentElement.children].filter(e => e.classList.contains('detail-content') && e !== t);
+let inputAuto = t => [...t.parentElement.children].filter(e => e.classList.contains('self') && e !== t);
+// 각 내용에 맞는 팝업창 열기
+watchDetail.forEach(item => item.addEventListener('click', e => {
+    const { id } = e.target;
+    detailArea.forEach(detail => {
+
+        if(detail.classList.contains(id)){
+            detail.classList.add('detail-toggle');
+            let cnt = 0; //직접입력 토글 카운트
+            // 내용 선택
+            detail.querySelectorAll('.detail-content').forEach(i => i.addEventListener('click', c => {
+                // 선택된 항목 색상 유지
+                i.classList.add('choice-content');
+                siblings(c.currentTarget).forEach(not => {if(not.classList.contains('choice-content')) not.classList.remove('choice-content')});
+                // 직접입력
+                if(i.classList.contains('self')){
+                    document.getElementById(id).value = "";
+                    cnt++;
+                    if(cnt%2 === 1)
+                        i.children[0].classList.add('input-toggle'); // 열림
+                    else if(c.target.className !== "self-input-box input-toggle" && c.target.className !== "self-input" && c.target.className !== "self-done")
+                        i.children[0].classList.remove('input-toggle'); // 닫힘
+                    // 직접입력 완료
+                    i.children[0].querySelector('.self-done').addEventListener('click', d => {
+                        d.stopImmediatePropagation();
+                        document.getElementById(id).value = i.children[0].querySelector('input').value;
+                        detail.classList.remove('detail-toggle');
+                    });
+                }else{
+                    document.getElementById(id).value = c.currentTarget.innerText;
+                    // 다른거 선택하면 직접입력 닫히게
+                    inputAuto(c.currentTarget).forEach(input => {
+                        input.children[0].classList.remove('input-toggle');
+                        input.children[0].querySelector('input').value = "";
+                    });
+                    detail.classList.remove('detail-toggle');
+                }
+            }));
+
+            //평가 별점 채우기
+            const starBox = document.querySelectorAll('.star-box');
+            let starNum = document.querySelector('.star-num');
+            detail.querySelectorAll('.star-wrap').forEach(item => item.addEventListener('click', s => {
+                let mouseX = s.offsetX;
+
+                if(s.currentTarget.id === "five" && mouseX >= 13 && mouseX <= 26){  // 5.0
+                    for(let i = 0; i < 5; i++){
+                        starBox[i].classList.add('full');
+                    }
+                    starNum.innerText = "5.0";
+                }else if(s.currentTarget.id === "five" && mouseX < 13){  // 4.5
+                    for(let i = 0; i < 5; i++){
+                        if(i >= 4)  starBox[i].classList.remove('full');
+                        else starBox[i].classList.add('full');
+                    }
+                    starBox[4].classList.add('half');
+                    starNum.innerText = "4.5";
+                }else if(s.currentTarget.id === "four" && mouseX >= 13 && mouseX <= 26){  // 4.0
+                    for(let i = 0; i < 5; i++){
+                        if(i >= 4){
+                            starBox[i].classList.remove('full');
+                            starBox[i].classList.remove('half');
+                        }else starBox[i].classList.add('full');
+                    }
+                    starNum.innerText = "4.0";
+                }else if(s.currentTarget.id === "four" && mouseX < 13){  // 3.5
+                    for(let i = 0; i < 5; i++){
+                        if(i >= 3){
+                            starBox[i].classList.remove('full');
+                            starBox[i].classList.remove('half');
+                        }else starBox[i].classList.add('full');
+                    }
+                    starBox[3].classList.add('half');
+                    starNum.innerText = "3.5";
+                }else if(s.currentTarget.id === "three" && mouseX >= 13 && mouseX <= 26){  // 3.0
+                    for(let i = 0; i < 5; i++){
+                        if(i >= 3){
+                            starBox[i].classList.remove('full');
+                            starBox[i].classList.remove('half');
+                        }else starBox[i].classList.add('full');
+                    }
+                    starNum.innerText = "3.0";
+                }else if(s.currentTarget.id === "three" && mouseX < 13){  // 2.5
+                    for(let i = 0; i < 5; i++){
+                        if(i >= 2){
+                            starBox[i].classList.remove('full');
+                            starBox[i].classList.remove('half');
+                        }else starBox[i].classList.add('full');
+                    }
+                    starBox[2].classList.add('half');
+                    starNum.innerText = "2.5";
+                }else if(s.currentTarget.id === "two" && mouseX >= 13 && mouseX <= 26){  // 2.0
+                    for(let i = 0; i < 5; i++){
+                        if(i >= 2){
+                            starBox[i].classList.remove('full');
+                            starBox[i].classList.remove('half');
+                        }else starBox[i].classList.add('full');
+                    }
+                    starNum.innerText = "2.0";
+                }else if(s.currentTarget.id === "two" && mouseX < 13){  // 1.5
+                    for(let i = 0; i < 5; i++){
+                        if(i >= 1){
+                            starBox[i].classList.remove('full');
+                            starBox[i].classList.remove('half');
+                        }else starBox[i].classList.add('full');
+                    }
+                    starBox[1].classList.add('half');
+                    starNum.innerText = "1.5";
+                }else if(s.currentTarget.id === "one" && mouseX >= 13 && mouseX <= 26){  // 1.0
+                    for(let i = 0; i < 5; i++){
+                        if(i >= 1){
+                            starBox[i].classList.remove('full');
+                            starBox[i].classList.remove('half');
+                        }else starBox[i].classList.add('full');
+                    }
+                    starNum.innerText = "1.0";
+                }else if(s.currentTarget.id === "one" && mouseX < 13){  // 1.0
+                    for(let i = 0; i < 5; i++){
+                            starBox[i].classList.remove('full');
+                            starBox[i].classList.remove('half');
+                    }
+                    starBox[0].classList.add('half');
+                    starNum.innerText = "0.5";
+                }
+            }));
+
+            // 별점 확인 버튼
+            const starDone = (done) => {
+                document.querySelector('.star-done').addEventListener('click', () => {
+                    if(detail.classList.contains('watchStar')){
+                        if(done === ""){
+                            document.getElementById(id).value = "";
+                        }else if(done === undefined){
+                            document.getElementById(id).value = starNum.innerText;
+                        }
+                        detail.classList.remove('detail-toggle');
+                    }
+                });
+            };
+            starDone();
+
+            // 별점 초기화 버튼
+            let reset = "";
+            document.querySelector('.star-reset').addEventListener('click', () => {
+                starNum.innerText = "0.0";
+                for(let i = 0; i < 5; i++){
+                    starBox[i].classList.remove('full');
+                    starBox[i].classList.remove('half');
+                }
+                starDone(reset);
+            });
+
+            // 외부 클릭하면 디테일 모달창 닫힘
+            window.addEventListener('click', close => {
+                if(!detail.children[0].contains(close.target) && close.target.className !== "watch-detail"){
+                    detail.classList.remove('detail-toggle');
+                }
+                // 별점 외부영역 클릭
+                if(detail.classList.contains('watchStar')){
+                    if(starNum.innerText === "0.0") document.getElementById(id).value = "";
+                    else document.getElementById(id).value = starNum.innerText;
+                }
+            });
+        }
+    });
+}));
+
+
+
+
+
+
+
 
--- src/main/webapp/WEB-INF/views/movieSearch.jsp
+++ src/main/webapp/WEB-INF/views/movieSearch.jsp
@@ -59,19 +59,19 @@
                         </div>
                         <div class="watch-info">
                             <img src="/static/images/where.png">
-                            <input type="text" placeholder="관람 장소" readonly>
+                            <input type="text" placeholder="관람 장소" class="watch-detail" id="watchPlace" readonly>
                         </div>
                         <div class="watch-info">
                             <img src="/static/images/who.png">
-                            <input type="text" placeholder="누구랑" readonly>
+                            <input type="text" placeholder="누구랑" class="watch-detail" id="watchPerson" readonly>
                         </div>
                         <div class="watch-info">
                             <img src="/static/images/rating.png">
-                            <input type="text" placeholder="평점" readonly>
+                            <input type="text" placeholder="평가" class="watch-detail" id="watchStar" readonly>
                         </div>
                         <div class="watch-info">
                             <img src="/static/images/folder.png">
-                            <input type="text" placeholder="앨범선택" readonly>
+                            <input type="text" placeholder="앨범선택" class="watch-detail" id="choiceAlbum" readonly>
                         </div>
                     </div>
                     <div class="review-write-box">
@@ -81,6 +81,64 @@
                         <button class="record-btn-cancel">취소</button>
                         <button class="record-btn-submit">저장</button>
                     </div>
+                </div>
+            </div>
+            <%-- 관람장소, 누구랑, 평점, 앨범선택 팝업창 --%>
+            <%-- 관람장소 --%>
+            <div class="record-detail-area watchPlace">
+                <div class="record-detail-box">
+                    <p class="detail-title">관람 장소</p>
+                    <div class="detail-content">집</div>
+                    <div class="detail-content">극장</div>
+                    <div class="detail-content self">
+                        직접입력
+                        <div class="self-input-box">
+                            <input type="text" class="self-input">
+                            <button class="self-done">완료</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <%--누구랑 --%>
+            <div class="record-detail-area watchPerson">
+                <div class="record-detail-box">
+                    <p class="detail-title">누구랑</p>
+                    <div class="detail-content">혼자</div>
+                    <div class="detail-content">친구</div>
+                    <div class="detail-content">가족</div>
+                    <div class="detail-content">애인</div>
+                    <div class="detail-content self">
+                        직접입력
+                        <div class="self-input-box">
+                            <input type="text" class="self-input">
+                            <button class="self-done">완료</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <%-- 평점 --%>
+            <div class="record-detail-area watchStar">
+                <div class="record-detail-box">
+                    <p class="detail-title">평가</p>
+                    <div class="star-container">
+                        <i class="fas fa-star star-wrap" id="one"><div class="star-box"><i class="fas fa-star review-star"></i></div></i>
+                        <i class="fas fa-star star-wrap" id="two"><div class="star-box"><i class="fas fa-star review-star"></i></div></i>
+                        <i class="fas fa-star star-wrap" id="three"><div class="star-box"><i class="fas fa-star review-star"></i></div></i>
+                        <i class="fas fa-star star-wrap" id="four"><div class="star-box"><i class="fas fa-star review-star"></i></div></i>
+                        <i class="fas fa-star star-wrap" id="five"><div class="star-box"><i class="fas fa-star review-star"></i></div></i>
+                        <div class="star-num">0.0</div>
+                    </div>
+                    <div class="star-button-box">
+                        <button class="star-reset">초기화</button>
+                        <button class="star-done">확인</button>
+                    </div>
+                </div>
+            </div>
+            <%-- 앨범선택 --%>
+            <div class="record-detail-area choiceAlbum">
+                <div class="record-detail-box">
+                    <p class="detail-title">앨범선택</p>
+                    <div class="detail-content">기본 앨범</div>
                 </div>
             </div>
         </section>
